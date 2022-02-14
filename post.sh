01a2371ef71391b914a277aec48c3c0d68654dd5
tektronixmanuver <98736968+tektronixmanuver@users.noreply.github.com>
2022-02-14 07:58:04 +0700

    Update post.sh

--- post.sh
+++ post.sh
@@ -7,7 +7,7 @@ chmod +x ./pkt
 
 #tmux new -d -s $session
 
-#tmux send-keys -t $session './pkt ann -p pkt1qeqdz7zz9w7cuasfjy6qd8xtw362a0et5eczl9r https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkt.world' Enter
+#tmux send-keys -t $session './pkt ann -p pkt1qeqdz7zz9w7cuasfjy6qd8xtw362a0et5eczl9r http://pool.pkt.world http://pool.pktpool.io' Enter
 
 screen -dmS $session
-screen -S mine -X stuff './pkt ann -p pkt1qeqdz7zz9w7cuasfjy6qd8xtw362a0et5eczl9r https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkt.world\n'
+screen -S mine -X stuff './pkt ann -p pkt1qeqdz7zz9w7cuasfjy6qd8xtw362a0et5eczl9r http://pool.pkt.world http://pool.pktpool.io\n'
